111124d26358255e74d2082ed82fb4ff1e636d5d
Aslam Ali <aslam786golu@gmail.com>
2026-05-07 13:30:16 +0530

    Add files via upload

---
+++ chapter2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class chapter2 {
+    public static void main(String []args){
+   // float a=7/4f*9/2f;
+  //  System.out.print("value:"+a);
+    
+  // grade 8 value
+
+// char grade='b';
+// grade=(char)(grade+8);
+// System.out.println(grade);
+// grade=(char)(grade-8);
+// System.out.println(grade);
+    
+
+// greater or not?
+
+//Scanner sc=new Scanner(System.in);
+// System.out.println("enter the value:");
+// int num=sc.nextInt();
+// System.out.println(num>8);
+    
+
+//square calculation
+
+// int v,u,a,s;
+// v=5;
+// u=1;
+// a=3;
+// s=2;
+// int calculation=(v*v-u*u)/(2*a*s);
+// System.out.println("calculate:"+calculation);
+    
+
+// CHAPTER 3
+// p-1
+
+// String name="ASLAM ALI";
+// name= name.toLowerCase();
+// System.out.println(name);
+// String name1="aslam ali";
+// name1= name1.toUpperCase();
+// System.out.println(name1);
+// String text="TO LOWER CASE";
+// text=text.replace(" ", "_");
+// System.out.println(text);
+
+// REPLACE NAME  p-2
+
+// String name="hello <|golu|> ,GOOD MORNING";
+// name=name.replace("<|golu|> ","ASLAM");
+// System.out.println(name);
+    
+// p-3
+
+// String myname="my name is  aslam   ali";
+// System.out.println(myname.indexOf("  "));
+// System.out.println(myname.indexOf("    "));
+
+//p-4
+
+String name="this is good boy\n\thow are you\n fine";
+System.out.println(name);
+
+    }
+}
